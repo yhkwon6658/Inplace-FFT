2582a537d2458386d3d54cda5100b22e9f0e6c94
yhkwon6658 <yonghwankwon6658@gmail.com>
2022-12-28 23:27:55 +0900

    Delete SRC directory

--- SRC/barrel_shifter.v
+++
@@ -1,7 +0,0 @@
-module barrel_shifter (
-    input s,
-    output w1, w0
-);
-    assign  w1 = s ? 1'b1 : 1'b0;
-    assign  w0 = s ? 1'b0 : 1'b1;
-endmodule
--- SRC/bwMUX.v
+++
@@ -1,9 +0,0 @@
-module bwMUX
-(
-    input in1,
-    input in2,  
-    input sel,
-    output out
-);
-assign out = sel ? in1 : in2;
-endmodule
